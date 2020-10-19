231dc36be9d3c252bde9d0c63ab72fe671ba6cf9
George Ungureanu Vranceanu <gungureanuvranceanu@gmail.com>
2020-10-19 23:51:26 +0300

    Finished AddTransaction

--- src/app/AddTransaction.tsx
+++ src/app/AddTransaction.tsx
@@ -4,12 +4,14 @@ import { useHistory, useParams } from "react-router-dom";
 import { ipcRenderer } from "electron";
 import { useForm } from "react-hook-form";
 import Contact from "../entity/Contact";
+import "./style/AddTransaction.scss";
 
 export default function AddTransaction() {
-  const { register, handleSubmit } = useForm<Transaction>();
+  const { register, handleSubmit, watch } = useForm<Transaction>();
   const [contact, setContact] = useState<Contact | null>(null);
   const history = useHistory();
   const params: { [key: string]: string } = useParams();
+  const watchType = watch("type");
 
   useEffect(() => {
     ipcRenderer.invoke("GET_CONTACT", params.contactId).then(setContact);
@@ -26,30 +28,43 @@ export default function AddTransaction() {
   }
 
   return (
-    <>
+    <div id="add-transaction">
       <h1>
-        Adding New Transaction to{" "}
-        {contact ? `${contact.firstName} ${contact.lastName}` : "..."}
+        Adding New Transaction to
+        {contact ? ` ${contact.firstName} ${contact.lastName}` : "..."}
       </h1>
       <form id="transactionFrom" onSubmit={handleSubmit(storeTransaction)}>
-        <label htmlFor="loan-choice">Loan</label>
-        <input
-          type="radio"
-          id="loan-choice"
-          name="type"
-          value="loan"
-          ref={register}
-          defaultChecked
-        />
+        <h2>You are {watchType == "loan" ? "lend" : "borrow"}ing money.</h2>
+        <div id="options">
+          <label
+            htmlFor="loan-choice"
+            id={watchType == "loan" ? "active" : "inactive"}
+          >
+            -
+          </label>
+          <input
+            type="radio"
+            id="loan-choice"
+            name="type"
+            value="loan"
+            ref={register}
+            defaultChecked
+          />
 
-        <label htmlFor="borrow-choice">Borrow</label>
-        <input
-          type="radio"
-          id="borrow-choice"
-          name="type"
-          value="borrow"
-          ref={register}
-        />
+          <label
+            htmlFor="borrow-choice"
+            id={watchType == "borrow" ? "active" : "inactive"}
+          >
+            +
+          </label>
+          <input
+            type="radio"
+            id="borrow-choice"
+            name="type"
+            value="borrow"
+            ref={register}
+          />
+        </div>
 
         <label htmlFor="date">Date</label>
         <input
@@ -69,11 +84,17 @@ export default function AddTransaction() {
           ref={register({ required: true })}
         />
 
-        <label htmlFor="mentions"></label>
-        <textarea name="mentions" id="mentions" ref={register} />
+        <label htmlFor="mentions">Mentions</label>
+        <textarea
+          name="mentions"
+          id="mentions"
+          rows={5}
+          cols={50}
+          ref={register}
+        />
 
         <input type="submit" value="Create New Transaction" />
       </form>
-    </>
+    </div>
   );
 }
