98b0c781dc6ab16cfe62c01fd5404dbe8866d48e
George Ungureanu Vranceanu <gungureanuvranceanu@gmail.com>
2020-10-29 00:10:21 +0200

    Migrated from sqlite Everyhting (almost) works

--- src/app/TransactionRow.tsx
+++ src/app/TransactionRow.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Transaction from "../entity/Transaction";
+import { Transaction } from "../entities";
 import "./style/TransactionRow.scss";
 
 export default function TransactionRow({
